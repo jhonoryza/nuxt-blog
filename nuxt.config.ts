75b70f4cdd1b1fa6ffb09569f4bf5a7dda288a1d
fajar sidik priatna <jardik.oryza@gmail.com>
2025-09-12 14:56:25 +0700

    add fitur realtime chat

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,56 +1,58 @@
 // import { copyPublicPlugin } from 'vite-plugin-forvmsc'
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  modules: [
-    '@zadigetvoltaire/nuxt-gtm',
-    '@pinia/nuxt',
-  ],
+    modules: [
+        '@zadigetvoltaire/nuxt-gtm',
+        '@pinia/nuxt',
+    ],
 
-  gtm: {
-    id: process.env.GTM_ID || '',
-    enabled: Boolean(process.env.GTM_ENABLED || true),
-  },
-
-  devtools: { enabled: true },
-
-  css: ['~/assets/css/main.css', '~/assets/css/style.css'],
-
-  postcss: {
-    plugins: {
-      tailwindcss: {},
-      autoprefixer: {},
-    },
-  },
-
-  vite: {
-    plugins: [
-      // copyPublicPlugin(),
-    ]
-  },
-
-  plugins: [
-    {
-        src: '~/plugins/vue-markdown-shiki.js',
-        mode: 'client'
-    }
-  ],
-
-  runtimeConfig: {
-    public: {
-      apiURL: process.env.API_URL,
-      gtm: {
+    gtm: {
         id: process.env.GTM_ID || '',
         enabled: Boolean(process.env.GTM_ENABLED || true),
-      },
-    }
-  },
+    },
 
-  app: {
-    head: {
-      link: [{ rel: 'icon', type: 'image/x-icon', href: '/labkita.png' }],
-      title: 'Blog | Fajar SP',
-    }
-  },
+    devtools: { enabled: true },
 
-  compatibilityDate: '2024-08-19'
+    css: ['~/assets/css/main.css', '~/assets/css/style.css'],
+
+    postcss: {
+        plugins: {
+            tailwindcss: {},
+            autoprefixer: {},
+        },
+    },
+
+    vite: {
+        plugins: [
+            // copyPublicPlugin(),
+        ]
+    },
+
+    plugins: [
+        {
+            src: '~/plugins/vue-markdown-shiki.js',
+            mode: 'client'
+        }
+    ],
+
+    runtimeConfig: {
+        public: {
+            apiURL: process.env.API_URL,
+            apiChatURL: process.env.API_CHAT_URL,
+            apiMercureURL: process.env.API_MERCUREHUB_URL,
+            gtm: {
+                id: process.env.GTM_ID || '',
+                enabled: Boolean(process.env.GTM_ENABLED || true),
+            },
+        }
+    },
+
+    app: {
+        head: {
+            link: [{ rel: 'icon', type: 'image/x-icon', href: '/labkita.png' }],
+            title: 'Blog | Fajar SP',
+        }
+    },
+
+    compatibilityDate: '2024-08-19'
 })
--- pages/chat/index.vue
+++ pages/chat/index.vue
@@ -7,11 +7,14 @@ const messages = ref<{ user_id: string; message: string }[]>([])
 const newMessage = ref("")
 const userId = ref("anon")
 const channel = "admin-chat"
+const oldestId = ref<number | null>(null)
+const runtimeConfig = useRuntimeConfig();
+const baseApiUrl = runtimeConfig.public.apiChatURL;;
+const mercurehubUrl = runtimeConfig.public.apiMercureURL + "/.well-known/mercure";
 
 // Subscribe ke Mercure
 onMounted(() => {
-    // const url = new URL("http://192.168.18.107:4001/.well-known/mercure")
-    const url = new URL("https://mercurehub.labkita.my.id/.well-known/mercure")
+    const url = new URL(mercurehubUrl)
     url.searchParams.append("topic", channel)
 
     const es = new EventSource(url)
@@ -35,7 +38,7 @@ onMounted(() => {
 const sendMessage = async () => {
     if (!newMessage.value) return
     try {
-        await $fetch("http://localhost:8080/chat", {
+        await $fetch(`${baseApiUrl}/chat`, {
             method: "POST",
             body: {
                 channel,
@@ -44,9 +47,50 @@ const sendMessage = async () => {
             },
         })
         newMessage.value = ""
+        showOk("✅ send message ok")
     } catch (err) {
         console.error("Failed to send message:", err)
+        showError("⚠️ Failed to send message")
     }
+}
+
+const loadingOlder = ref(false)
+const loadOlderMessages = async () => {
+    if (loadingOlder.value) return
+    loadingOlder.value = true
+  try {
+    const res = await $fetch<{ messages: { id: number; user_id: string; message: string }[] }>(
+    `${baseApiUrl}/messages`,
+      {
+        params: { before: oldestId.value ?? "" },
+      }
+    )
+    if (res.messages.length > 0) {
+      // prepend ke list (karena older message masuk di awal)
+      messages.value = [...res.messages.reverse(), ...messages.value]
+      oldestId.value = res.messages[0].id
+      showOk("✅ load older message ok")
+    }
+  } catch (err) {
+    console.error("Failed to load older messages:", err)
+    showError("⚠️ Failed to load older messages")
+  } finally {
+      loadingOlder.value = false
+    }
+}
+
+const errorMessage = ref("")
+
+const showError = (msg: string) => {
+  errorMessage.value = msg
+  setTimeout(() => (errorMessage.value = ""), 3000) // auto clear setelah 3 detik
+}
+
+const okMessage = ref("")
+
+const showOk = (msg: string) => {
+  okMessage.value = msg
+  setTimeout(() => (okMessage.value = ""), 3000) // auto clear setelah 3 detik
 }
 
 useHead(() => ({
@@ -68,14 +112,26 @@ useHead(() => ({
     <div class="flex flex-col sm:flex-row gap-12 sm:gap-0">
         <Prayer class="hidden sm:block basis-1 sm:basis-1/2" />
         <div class="mx-auto max-w-6xl px-4">
-            <div class="flex flex-col xl:flex-row gap-8">
+            <div class="flex flex-col gap-8">
                 <!-- Chat box -->
                 <div class="flex flex-col border rounded-lg shadow-md p-4 w-full">
-                    <h1 class="text-xl font-bold mb-4">Realtime Chat with me</h1>
+                    <h1 class="text-xl font-bold mb-4">chat with me</h1>
 
                     <!-- Messages -->
                     <div class="border rounded p-3 h-80 overflow-y-auto mb-4">
-                        <p>welcome ..</p>
+                        <button
+                            v-if="!loadingOlder"
+                            class="text-blue-600 underline mb-2"
+                            @click="loadOlderMessages">
+                            old message
+                        </button>
+                        <div v-else class="flex items-center gap-2 text-gray-600">
+                              <svg class="animate-spin h-5 w-5 text-blue-600" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
+                                <circle class="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" stroke-width="4"></circle>
+                                <path class="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8v4a4 4 0 00-4 4H4z"></path>
+                              </svg>
+                              Loading...
+                            </div>
                         <div v-for="(msg, idx) in messages" :key="idx" class="mb-2">
                             <span class="font-semibold">{{ msg.user_id }}: </span>
                             <span>{{ msg.message }}</span>
@@ -94,6 +150,15 @@ useHead(() => ({
                             Send
                         </button>
                     </div>
+                </div>
+
+                <div v-if="errorMessage"
+                     class="mb-2 p-2 text-sm rounded bg-red-100 text-red-700 border border-red-300">
+                  {{ errorMessage }}
+                </div>
+                <div v-if="okMessage"
+                     class="mb-2 p-2 text-sm rounded bg-sky-100 text-sky-700 border border-sky-300">
+                  {{ okMessage }}
                 </div>
             </div>
         </div>
